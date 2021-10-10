ce96836b139b51ed34178c4598d99c6da511a68a
nrblt <44442477+nrblt@users.noreply.github.com>
2021-10-10 19:26:40 +0600

    Create test.py

---
+++ test/test.py
@@ -0,0 +1,9 @@
+from main import ParseArticle
+
+
+par=ParseArticle();
+par.scrapInformation('bitcoin')
+par.scrapInformation('cardano')
+# k=input('Please write crypto currency name: ')
+# par.scrapInformation(k)
+
